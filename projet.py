1a2be2522596555e16f5fe93b4e52191e0e96ad3
Alexis Hucteau <ahucteau@emi.u-bordeaux1.fr>
2020-01-29 13:31:17 +0100

    Import des fichiers métabo et ajout des metrics sur les noeuds et arêtes

--- projet.py
+++ projet.py
@@ -31,7 +31,7 @@ def importData():
     i +=1
   return interraction_dict
 
-def importData2(nodes):
+def import_chromosome6_fragments_expressions(nodes, expression):
   wd = '/autofs/unityaccount/cremi/ahucteau/Semestre_9/DEA/Tulip_projet/Projet_DEA/'
   file = 'chromosome6_fragments_expressions.csv'
   with open(wd+file, newline='') as csvfile:
@@ -40,21 +40,48 @@ def importData2(nodes):
   lenread=len(read)
   for ligne in range(lenread):
     if read[ligne][1] in nodes.keys():
-      nodes[read[ligne][1]]['expression']=read[ligne][2]
+      nodes[read[ligne][1]]["expression"]=read[ligne][2]
+
+def import_metabos(nodes, edges):
+  wd = '/autofs/unityaccount/cremi/ahucteau/Semestre_9/DEA/Tulip_projet/Projet_DEA/'
+  file = 'KEGG.symbols.csv'
+  with open(wd+file, newline='') as csvfile:
+    read=list(csv.reader(csvfile, delimiter='\t'))
+  for ligne in range(len(read)):
+    for index_gene in range(2,len(read[ligne])):
+      if read[ligne][index_gene] in nodes.keys():
+        nodes[read[ligne][index_gene]]["metabo"].append(read[ligne][0])
+  file = 'REACTOME.symbols.csv'
+  with open(wd+file, newline='') as csvfile:
+    read=list(csv.reader(csvfile, delimiter='\t'))
+  for ligne in range(len(read)):
+    for index_gene in range(2,len(read[ligne])):
+      if read[ligne][index_gene] in nodes.keys():
+        nodes[read[ligne][index_gene]]["reactome"].append(read[ligne][0])
 
 def create_nodes_edges(gr,dico,nodes):
   for key in dico.keys():
     locus=dico[key]["locus1"]
     if locus not in nodes.keys():
       nodes[locus]={}
-      nodes[locus]={'node':gr.addNode()}
+      nodes[locus]={"node":gr.addNode(), "metabo":[], "reactome":[]}
     locus2=dico[key]["locus2"]
     if locus2 not in nodes.keys():
       nodes[locus2]={}
-      nodes[locus2]={'node':gr.addNode()}
-    dico[key]['edge']=gr.addEdge(nodes[locus]['node'],nodes[locus2]['node'])
+      nodes[locus2]={"node":gr.addNode(), "metabo":[], "reactome":[]}
+    dico[key]["edge"]=gr.addEdge(nodes[locus]["node"],nodes[locus2]["node"])
 
-
+def ajouter_metrics(nodes, metrics, edges):
+  for node in nodes.keys():
+    metrics["expression"][nodes[node]["node"]]=nodes[node]["expression"]
+    if len(nodes[node]["reactome"])!=0:
+      metrics["reactome"][nodes[node]["node"]]=nodes[node]["reactome"]
+    if len(nodes[node]["metabo"])!=0:
+      metrics["metabo"][nodes[node]["node"]]=nodes[node]["metabo"]
+  for edge in edges.keys():
+    metrics["distance"][edges[edge]["edge"]]=int(edges[edge]["distance"])
+    metrics["interraction"][edges[edge]["edge"]]=edges[edge]["interraction"]
+  return 0
 
 # The updateVisualization(centerViews = True) function can be called
 # during script execution to update the opened views
@@ -70,18 +97,7 @@ def create_nodes_edges(gr,dico,nodes):
 # to run the script on the current graph
 
 def main(graph):
-  locus1 = []
-  locus2 = []
-  interaction = []
-  distances = []
-  nodes = {}
-  edges = {}
-  interract_dict = {}
-
-  interraction_dict=importData()
-  create_nodes_edges(graph, interraction_dict, nodes)
-  importData2(nodes)
-
+  '''
   viewBorderColor = graph.getColorProperty("viewBorderColor")
   viewBorderWidth = graph.getDoubleProperty("viewBorderWidth")
   viewColor = graph.getColorProperty("viewColor")
@@ -104,5 +120,26 @@ def main(graph):
   viewTexture = graph.getStringProperty("viewTexture")
   viewTgtAnchorShape = graph.getIntegerProperty("viewTgtAnchorShape")
   viewTgtAnchorSize = graph.getSizeProperty("viewTgtAnchorSize")
+  '''
+  
+  Metrics={}
+  Metrics["expression"]=graph.getStringProperty("expression")
+  Metrics["distance"]=graph.getDoubleProperty("distance")
+  Metrics["interraction"]=graph.getStringProperty("interaction")
+  Metrics["reactome"]=graph.getStringVectorProperty("reactome")
+  Metrics["metabo"]=graph.getStringVectorProperty("metabo")
 
+  locus1 = []
+  locus2 = []
+  interaction = []
+  distances = []
+  nodes = {}
+  edges = {}
+  interract_dict = {}
 
+  interraction_dict=importData()
+  create_nodes_edges(graph, interraction_dict, nodes)
+  import_chromosome6_fragments_expressions(nodes, Metrics["expression"])
+  import_metabos(nodes, interraction_dict)
+  ajouter_metrics(nodes, Metrics, interraction_dict)  
+  
